c02576da57f0f1d9a34c72bcda82d38774576d41
Valeria Medina <valeriam200305@gamil.com>
2025-02-18 23:21:54 -0500

    Conceptos

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("git config --global user.name");
+        System.out.println("git config --global user.email");
+        System.out.println("git config --list");
+        System.out.println("git init");
+        System.out.println("git status");
+        System.out.println("git log");
+        System.out.println("git add .");
+        System.out.println("git commit ");
+        System.out.println("git push");
+        System.out.println("git pull");
+        System.out.println("git switch -c");
+        System.out.println("git switch");
+        System.out.println("git merge");
+        System.out.println("git log");
+        System.out.println("git branch -D");
+        System.out.println("git branch --delete");
+        System.out.println("como devolver cambios de la rama");
+    }
+}
